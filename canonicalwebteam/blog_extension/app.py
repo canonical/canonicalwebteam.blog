9b0d16ad4c410c099dd1d3a1b6cb7abf1dc26cf8
Thomas Bille <thomas.bille@canonical.com>
2019-02-13 12:17:24 +0000

    Apply black

--- canonicalwebteam/blog_extension/app.py
+++ canonicalwebteam/blog_extension/app.py
@@ -2,7 +2,14 @@ from webapp.blog_ext.views import build_blueprint
 
 
 class BlogExtension(object):
-    def __init__(self, app=None, blog_title=None, tag_id=None, tag_name=None, url_prefix=None):
+    def __init__(
+        self,
+        app=None,
+        blog_title=None,
+        tag_id=None,
+        tag_name=None,
+        url_prefix=None,
+    ):
         self.app = app
         if app is not None:
             self.init_app(app, blog_title, tag_id, tag_name, url_prefix)
