80b209ce9a0169cdf3c9b2adb712cf12f830370a
Max <2843450+b-m-f@users.noreply.github.com>
2019-02-20 16:43:24 +0000

    Bumps version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.blog",
-    version="0.1.3",
+    version="0.1.4",
     author="Canonical webteam",
     author_email="webteam@canonical.com",
     url="https://github.com/canonical-webteam/blog-flask-extension",
