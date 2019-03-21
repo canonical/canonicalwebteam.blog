3be776d09630d25377fd8c74e125bf8d37e08e1d
b-m-f <max@ehlers.berlin>
2019-03-21 11:58:51 +0000

    Updates version to 1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.blog",
-    version="0.1.4",
+    version="1.0.0",
     author="Canonical webteam",
     author_email="webteam@canonical.com",
     url="https://github.com/canonical-webteam/blog-extension",
