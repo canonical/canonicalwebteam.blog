41437913a0db1b9803b09fa36726f9e69e9eb32f
b-m-f <max@ehlers.berlin>
2019-04-08 13:35:46 +0100

    Remove import of Django app on blog __init__.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.blog",
-    version="1.3.0",
+    version="1.3.1",
     author="Canonical webteam",
     author_email="webteam@canonical.com",
     url="https://github.com/canonical-webteam/blog-extension",
