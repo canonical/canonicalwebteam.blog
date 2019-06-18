0102cdc8b2ecc87e895254874c5230748cc002e0
b-m-f <max@ehlers.berlin>
2019-06-18 11:27:41 +0100

    Make build_url more generic to be used by other functions as well. Removing get_articles_with_metadata and only have a single function that returns metadata in addition to normal payload

--- tests/test_django_views.py
+++ tests/test_django_views.py
@@ -1,10 +1,10 @@
 # Core
 import os
 import unittest
-from unittest.mock import patch
 
 # Packages
 import django
+import vcr
 from django.conf import settings
 from django.test import Client
 
@@ -32,47 +32,10 @@ mock_article = {"id": 1}
 
 
 class TestDjangoViews(unittest.TestCase):
-    def setUp(self):
-        get_articles_mock = patch(
-            "canonicalwebteam.blog.wordpress_api.get_articles",
-            return_value=tuple([[mock_article], "1"]),
-        )
-        get_index_context_mock = patch(
-            "canonicalwebteam.blog.common_view_logic.get_index_context",
-            return_value={},
-        )
-
-        # https://docs.python.org/3/library/unittest.mock.html#patch-methods-start-and-stop
-        self.get_articles_patch = get_articles_mock.start()
-        self.get_index_context_patch = get_index_context_mock.start()
-        self.addCleanup(patch.stopall)
-
-    def test_homepage(self,):
+    @vcr.use_cassette("fixtures/vcr_cassettes/django_homepage.yaml")
+    def test_homepage(self):
 
         django_client = Client()
 
-        django_client.get("/")
-
-        self.assertEqual(self.get_articles_patch.call_count, 3)
-        self.get_index_context_patch.assert_called_once_with(
-            "1",
-            [mock_article],
-            "1",
-            featured_articles=[mock_article],
-            upcoming=[mock_article],
-        )
-
-    # TODO: This test has a problem with the patching method
-    # It fails due to a mismatch on the patched method
-    # between test function and application funcion.
-    # It needs a through debugging, which is for now descoped
-    def ignore_test_second_page(self):
-
-        django_client = Client()
-
-        django_client.get("/?page=2")
-
-        self.get_articles_patch.assert_called_once()
-        self.get_index_context_patch.assert_called_once_with(
-            "2", [mock_article], "2", featured_articles=[]
-        )
+        response = django_client.get("/")
+        self.assertEqual(response.status_code, 200)
--- tests/test_wordpress_api.py
+++ tests/test_wordpress_api.py
@@ -30,7 +30,7 @@ class TestWordPressApi(unittest.TestCase):
     def test_get_articles_with_metadata(self, get):
         get.return_value = SuccessMockResponse()
 
-        articles, metadata = api.get_articles_with_metadata()
+        articles, metadata = api.get_articles()
         self.assertIsNotNone(metadata["total_pages"])
         self.assertIsNotNone(metadata["total_posts"])
 
@@ -44,8 +44,9 @@ class TestWordPressApi(unittest.TestCase):
     def test_get_articles(self, get):
         get.return_value = SuccessMockResponse()
 
-        _, total_pages = api.get_articles()
-        self.assertEqual(total_pages, "12")
+        _, metadata = api.get_articles()
+        self.assertEqual(metadata["total_pages"], "12")
+        self.assertEqual(metadata["total_posts"], "52")
         get.assert_called_once_with(
             "https://admin.insights.ubuntu.com/"
             "wp-json/wp/v2/posts?"
@@ -69,6 +70,7 @@ class TestWordPressApi(unittest.TestCase):
 
         get.assert_called_once_with(
             "https://admin.insights.ubuntu.com/wp-json/wp/v2/users/217"
+            "&_embed"
         )
 
     @patch("canonicalwebteam.blog.wordpress_api.api_session.get")
@@ -78,6 +80,7 @@ class TestWordPressApi(unittest.TestCase):
         get.assert_called_once_with(
             "https://admin.insights.ubuntu.com/"
             "wp-json/wp/v2/users?slug=canonical"
+            "&_embed"
         )
 
     @patch("canonicalwebteam.blog.wordpress_api.api_session.get")
@@ -86,6 +89,7 @@ class TestWordPressApi(unittest.TestCase):
         _ = api.get_media(id)
         get.assert_called_once_with(
             "https://admin.insights.ubuntu.com/wp-json/wp/v2/media/89203"
+            "&_embed"
         )
 
     @patch("canonicalwebteam.blog.wordpress_api.api_session.get")
@@ -93,7 +97,8 @@ class TestWordPressApi(unittest.TestCase):
         slug = "snappy"
         _ = api.get_tag_by_slug(slug)
         get.assert_called_once_with(
-            "https://admin.insights.ubuntu.com/wp-json/wp/v2/tags/?slug=snappy"
+            "https://admin.insights.ubuntu.com/wp-json/wp/v2/tags?slug=snappy"
+            "&_embed"
         )
 
     @patch("canonicalwebteam.blog.wordpress_api.api_session.get")
@@ -102,6 +107,7 @@ class TestWordPressApi(unittest.TestCase):
         _ = api.get_category_by_id(id)
         get.assert_called_once_with(
             "https://admin.insights.ubuntu.com/wp-json/wp/v2/categories/1453"
+            "&_embed"
         )
 
     @patch("canonicalwebteam.blog.wordpress_api.api_session.get")
@@ -111,6 +117,7 @@ class TestWordPressApi(unittest.TestCase):
         get.assert_called_once_with(
             "https://admin.insights.ubuntu.com/"
             "wp-json/wp/v2/categories?slug=articles"
+            "&_embed"
         )
 
     @patch("canonicalwebteam.blog.wordpress_api.api_session.get")
@@ -119,6 +126,7 @@ class TestWordPressApi(unittest.TestCase):
         get.assert_called_once_with(
             "https://admin.insights.ubuntu.com/"
             "wp-json/wp/v2/categories?per_page=100"
+            "&_embed"
         )
 
     @patch("canonicalwebteam.blog.wordpress_api.api_session.get")
@@ -127,6 +135,7 @@ class TestWordPressApi(unittest.TestCase):
         _ = api.get_group_by_id(id)
         get.assert_called_once_with(
             "https://admin.insights.ubuntu.com/wp-json/wp/v2/group/3367"
+            "&_embed"
         )
 
     @patch("canonicalwebteam.blog.wordpress_api.api_session.get")
@@ -135,6 +144,7 @@ class TestWordPressApi(unittest.TestCase):
         _ = api.get_group_by_slug(slug)
         get.assert_called_once_with(
             "https://admin.insights.ubuntu.com/wp-json/wp/v2/group?slug=ai"
+            "&_embed"
         )
 
     @patch("canonicalwebteam.blog.wordpress_api.api_session.get")
@@ -143,7 +153,8 @@ class TestWordPressApi(unittest.TestCase):
         _ = api.get_tags_by_ids(ids)
         get.assert_called_once_with(
             "https://admin.insights.ubuntu.com/"
-            "wp-json/wp/v2/tags?include=2080,2655"
+            "wp-json/wp/v2/tags?include=2080%2C2655"
+            "&_embed"
         )
 
     @patch("canonicalwebteam.blog.wordpress_api.api_session.get")
@@ -152,4 +163,5 @@ class TestWordPressApi(unittest.TestCase):
         _ = api.get_tag_by_name(name)
         get.assert_called_once_with(
             "https://admin.insights.ubuntu.com/wp-json/wp/v2/tags?search=10.04"
+            "&_embed"
         )
